bd9b708d134134b766a7b7c5c2c9feeb1f80aa01
ac2pic <ac2pic@gmail.com>
2022-02-04 03:54:57 -0600

    Update Cmake generator script and fix CMake template bugs

--- gen_cmake.py
+++ gen_cmake.py
@@ -3,17 +3,37 @@
 import sys
 tmpl = sys.argv[1]
 output = ""
+def execCode(code):
+    tmpLocal = {}
+    exec(code, globals(), tmpLocal)
+    return tmpLocal.get("result", "")
+
+def resolveLine(line):
+    originalLine = line
+    generated = ""
+    codeStart = "<py:"
+    codeEnd = ":py>"
+    while True:
+        startIndex = line.find(codeStart)
+        if startIndex == -1:
+            generated += line
+            break
+        generated += line[:startIndex]
+
+        startIndex += len(codeStart)
+        endIndex = line.find(codeEnd, startIndex)
+        if endIndex == -1:
+            generated = originalLine
+            break
+        code = line[startIndex: endIndex]
+        print(code)
+        generated += execCode(code)        
+        line = line[endIndex + len(codeEnd):]
+    return generated
+    
 with open(tmpl, 'r') as tmplFile:
     for line in tmplFile:
-        if line.strip().startswith("#python:"):
-            line = line.strip()
-            codeToExec = line[len("#python:"):]
-            codeToExec = codeToExec.strip()
-            tmpLocal = {}
-            exec(codeToExec, globals(), tmpLocal)
-            output += tmpLocal.get("result", "") + "\n"
-        else:
-            output += line + "\n"
+       output += resolveLine(line) + "\n" 
 
 with open('CMakeLists.txt', 'w') as outFile:
     outFile.write(output)
