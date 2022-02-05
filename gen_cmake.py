87515bac58163471b6cd2c7b68f518171e5cf2c6
ac2pic <ac2pic@gmail.com>
2022-02-04 18:18:10 -0600

    gen_cmake.py: Add support external code execution

--- gen_cmake.py
+++ gen_cmake.py
@@ -1,12 +1,56 @@
 #!/usr/bin/python3 
-
+import ast
 import sys
 tmpl = sys.argv[1]
 output = ""
-def execCode(code):
-    tmpLocal = {}
-    exec(code, globals(), tmpLocal)
-    return tmpLocal.get("result", "")
+def getCodeLine(statement,code):
+    lines = code.split("\n")
+    startLine = statement.lineno - 1
+    endLine = statement.end_lineno - 1
+    colStart = statement.col_offset
+    colEnd = statement.end_col_offset
+                      
+    if startLine == endLine:
+        return lines[startLine][colStart:colEnd]
+    codeLines = ""
+    codeLines += lines[startLine][colStart:] + "\n"
+    for line in lines[startLine + 1:endLine]:
+        codeLines += line + "\n"
+    codeLines += lines[endLine][:colEnd] + "\n"
+    return codeLines
+
+def dofile(name):
+    searchPaths = [
+        '',
+        'scripts/'
+    ]
+    for searchPath in searchPaths:
+        try:
+            
+            with open('{}{}.py'.format(searchPath, name), 'r') as execFile:
+                return execCode(execFile.read())
+        except FileNotFoundError:
+            pass
+    raise Exception('Could not find {}'.format(name))
+
+def execCode(code, session_locals = locals()):
+    predef_globals = globals()
+    session_locals = session_locals.update({
+        'dofile': dofile,
+    })
+    code_ast = ast.parse(code)
+    nodes = ast.iter_child_nodes(code_ast)
+    statements = list(nodes)
+    if len(statements) == 0:
+        return None
+    if not isinstance(statements[-1], ast.Expr):
+        raise TypeError("Expected statement but got:\n{}".format(getCodeLine(statements[-1], code)))
+
+    if len(statements) > 1:
+        compiled_statements = compile(ast.Module(body=statements[:-1], type_ignores=[]), "<ast>", "exec")
+        exec(compiled_statements, predef_globals, session_locals)
+    compiled_line = compile(ast.Expression(statements[-1].value), "<ast>", "eval")
+    return eval(compiled_line, predef_globals, session_locals)
 
 def resolveLine(line):
     originalLine = line
@@ -26,7 +70,6 @@ def resolveLine(line):
             generated = originalLine
             break
         code = line[startIndex: endIndex]
-        print(code)
         generated += execCode(code)        
         line = line[endIndex + len(codeEnd):]
     return generated
