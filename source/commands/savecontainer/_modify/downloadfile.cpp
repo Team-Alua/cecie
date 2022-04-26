43e31eb8f604994ee62af3ba6a74a5d70e8a700e
ac2pic <ac2pic@gmail.com>
2022-04-26 17:43:39 -0500

    Refactor

--- source/commands/savecontainer/_modify/downloadfile.cpp
+++ source/commands/savecontainer/_modify/downloadfile.cpp
@@ -7,6 +7,8 @@
 
 #include "filesystem.hpp"
 
+#include <sys/stat.h>
+
 void DownloadFileFromSaveContainerCommand::Execute(Network & network, int & sessionIndex, Sessions & sessions) {
 	ClientSession * clientSession = sessions.Get(sessionIndex);
 
@@ -22,7 +24,19 @@ void DownloadFileFromSaveContainerCommand::Execute(Network & network, int & sess
 	char mountFolder[256];
 	memset(mountFolder, 0, sizeof(mountFolder));
 	sprintf(mountFolder, "/mnt/sandbox/%s_000%s/%s", TITLE_ID, clientSession->mountPath,  upload.path);
-	ssize_t uploadResult = network.uploadFile(mountFolder);
+
+	struct stat buff;
+	memset(&buff, 0, sizeof(struct stat));
+	int stat_result = stat(mountFolder, &buff);
+	if (stat_result == -1) {
+		// log error
+		CommandSystemStates state = CommandSystemStates::Done;
+		this->Notify(EventNamePhaseChange, &state);
+		return;
+	}
+	network.writeFull(&buff.st_size);
+	ssize_t uploadResult = network.uploadFile(mountFolder, buff.st_size);
+
 	if (uploadResult == -1) {
 		// log error
 		CommandSystemStates state = CommandSystemStates::Done;
